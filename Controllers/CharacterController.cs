85b05c58e7a9915e8005a1e0ed1b4b34d3dc8bfe
CAMGSMKH\nmanith <nmanith@cellcard.com.kh>
2023-06-20 15:31:41 +0700

    FirstCode

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace APILeaning.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+
+        private static List<Character> Characters = new List<Character>()
+        {
+            new Character(),
+            new Character {   Id=1 ,Name = "Manith"},
+            new Character {Id=2, Name = "Vatey" , Intelligence = 100},
+
+        };
+
+        [HttpGet("Get ALl ID")]
+        public ActionResult<Character> Get()
+        {
+            return Ok(Characters);
+        }
+
+        [HttpGet("Get Single ID ")]
+        public ActionResult<Character> GetSingleID(int id)
+        {
+            return Ok(Characters.FirstOrDefault(c => c.Id == id)); 
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newcharacter)
+        {
+            Characters.Add(newcharacter);
+            return Ok(Characters);
+        }
+
+
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace APILeaning.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight = 1,
+
+        Mage  =2,
+
+        Cleric = 3
+
+    }
+}
